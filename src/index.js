e0da5d8cf99743e230aaf7d8a003311d23d12c6a
Shiraz Esat <Shiraz.Esat@intercede.com>
2018-11-28 16:09:40 +0000

    File missing from last commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 
 import Header from './Header';
+import History from './History';
 
-import Button from '@material-ui/core/Button';
-import Collapse from '@material-ui/core/Collapse';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
@@ -78,82 +77,46 @@ class Game extends React.Component {
                 row: null,
                 lastPlayed: null,
             }],
-            stepNumber: 0,
-            xIsNext: true,
             reverseOrder: false,
         };
     }
 
     render() {
         const history = this.state.history;
-        const current = history[this.state.stepNumber];
+        const current = history[this.props.step];
         const winner = calculateWinner(current.squares);
-        const order = this.state.reverseOrder ? "reverseOrder" : "";
-        const historyList = !!history[1];
-
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
-            const location = step.col ? "(" + step.col + "," + step.row + ")" : "";
-            if (!historyList) return "";
-            return (
-                <li key={move}>
-                    <Button variant="outlined" color="secondary" onClick={() => this.jumpTo(move)}>{desc}</Button> {location}
-                </li>
-            )
-        })
+        const xIsNext = (this.props.step % 2) === 0;
 
         let status;
         if (winner) {
             status = 'Winner: ' + winner.name;
-        } else if (this.state.stepNumber === 9) {
+        } else if (this.props.step === 9) {
             status = 'Game Tie!';
         } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (xIsNext ? 'X' : 'O');
         }
 
         store.dispatch({ type: "SET_HEADER", payload: status });
+        store.dispatch({ type: "SET_HISTORY", payload: history });
 
         return (
-            <>
-            <Grid item sm={6} xs={12}>
-                <Board
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
-                    lastPlayed={current.lastPlayed}
-                    winningLine={winner ? winner.line : null}
-                />
-            </Grid>
-            <Grid item sm={6} xs={12}>
-                <Collapse in={historyList}>
-                    <Button variant="contained" color="primary" onClick={() =>
-                        this.setState({
-                            reverseOrder: !this.state.reverseOrder,
-                        })
-                    }>Toggle History Order</Button>
-                </Collapse>
-                <ol className={order}>{moves}</ol>
-            </Grid>
-            </>
+            <Board
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i, xIsNext)}
+                lastPlayed={current.lastPlayed}
+                winningLine={winner ? winner.line : null}
+            />
         );
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        });
-    }
-
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    handleClick(i, xIsNext) {
+        const history = this.state.history.slice(0, this.props.step + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        squares[i] = xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
                 squares: squares,
@@ -161,23 +124,36 @@ class Game extends React.Component {
                 row: parseInt(i / 3) + 1,
                 lastPlayed: i,
             }]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
         });
+        store.dispatch({ type: "SET_STEP", payload: history.length });
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        step: state.step
+    };
+}
+
+Game = connect(mapStateToProps)(Game);
+
 const initialState = {
-    status: ""
+    status: "",
+    history: [],
+    step: 0,
 };
 
 function reducer(state = initialState, action) {
-    if (action.type === "SET_HEADER") {
-        return {
-            status: action.payload
-        }
+    switch (action.type) {
+        case "SET_HEADER":
+            return { ...state, status: action.payload }
+        case "SET_HISTORY":
+            return { ...state, history: action.payload }
+        case "SET_STEP":
+            return { ...state, step: action.payload }
+        default:
+            return state;
     }
-    return state;
 }
 
 const store = createStore(reducer);
@@ -190,7 +166,12 @@ const App = () => (
         <div style={{ padding: 10 }} />
         <Paper>
             <Grid container spacing={8}>
-                <Game />
+                <Grid item sm={6} xs={12}>
+                    <Game />
+                </Grid>
+                <Grid item sm={6} xs={12}>
+                    <History />
+                </Grid>
             </Grid>
         </Paper>
     </MuiThemeProvider>
